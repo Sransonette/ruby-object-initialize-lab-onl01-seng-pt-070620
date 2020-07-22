5bcd207a2bfccf8c58d8cfa8d34549a78fd3f6f8
Sean Ransonette <sransonette@gmail.com>
2020-07-22 00:04:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog 
-  def initialize(dogs_name, dogs_breed)
+  def initialize(dogs_name, dogs_breed = "Mutt")
     @name = dogs_name
     @breed = dogs_breed
   end
   
-  def dogs_name=(dogs_name, dogs_breed)
+  def dogs_name=(dogs_name, dogs_breed = "Mutt")
     @name = dogs_name
     @breed = dogs_breed 
   end
@@ -18,21 +18,3 @@ class Dog
   end
 end
 
-class Dog
-  def initialize (dogs_name, dogs_breed = "Mutt")
-    @name = dogs_name
-    @breed = dogs_breed
-  end
-
-  def name=(dogs_name, dogs_breed = "Mutt")
-    @name = dogs_name
-    @breed = dogs_breed
-  end
-
-  def name
-    @name
-  end
-  def breed
-    @breed
-  end 
-end
